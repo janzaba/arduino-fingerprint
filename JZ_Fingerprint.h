07754b396038224dcb0bc12e946b4e56c64b8c96
Jan Żaba <contact@janzaba.pl>
2017-09-21 22:47:26 +0200

    Add class JZ_Fingerprint sceleton

---
+++ JZ_Fingerprint.h
@@ -0,0 +1,19 @@
+#ifndef JZ_FINGERPRINT_H
+#define JZ_FINGERPRINT_H
+
+#include <Arduino.h>
+
+typedef uint8_t  u8;
+
+
+class JZ_Fingerprint {
+
+public:
+
+
+private:
+
+
+};
+
+#endif /*JZ_FINGERPRINT_H*/
